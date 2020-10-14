ea03522248c6d05cb5d54af86fc5b12e362533ab
Queary <72671469+Queary@users.noreply.github.com>
2020-10-14 19:03:07 +0300

    edit reg:mail=email

--- JS/Lib/redu.js
+++ JS/Lib/redu.js
@@ -1,1 +1,24 @@
 //alert("redu")
+
+//глоба
+function doc(ele){
+  this.s = {hidden(){
+			return ele.style.display = "none"
+
+		},
+}
+
+
+
+
+
+
+
+//одиночные методы
+function print(content){
+  return console.log(content)
+}
+
+function redu_v(){
+  return print("developer edition:vo.1")
+}
