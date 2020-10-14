1b0819256d6a54e0d54bfc7744e078b68aaeac15
keeling8 <72690604+keeling8@users.noreply.github.com>
2020-10-14 18:57:42 +0300

    Окно регистрации

---
+++ registration.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+    <title>Регистрация</title>
+    </head>
+    <body>
+    <h2>Регистрация</h2>
+      <p>
+          <label>Ваш логин:<br></label>
+          <input name="login" type="text" size="15" maxlength="15">
+      </p>
+      <p>
+          <label>Ваш пароль:<br></label>
+          <input name="password" type="password" size="15" maxlength="15">
+      </p>
+      <p>
+          <label>Ваш псевдоним:<br></label>
+          <input name="nickname" type="nickname" size="15" maxlength="15">
+      </p>
+      <p>
+          <label>Ваш mail:<br></label>
+          <input name="mail" type="mail" size="15" maxlength="15">
+      </p>
+      <p>
+          <input type="submit" name="submit" value="Зарегистрироваться">
+      </p>
+    </body>
+    </html>
