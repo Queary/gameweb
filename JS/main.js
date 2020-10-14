69e19faba4799926f49d2785991a574fea42e80d
Queary <72671469+Queary@users.noreply.github.com>
2020-10-14 19:54:20 +0300

    откоментил разные подходы к реализации события show

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,17 @@
 //открытые окна рега
 function showwindow(){
+  //реализации
+  //redu
   doc("window").s.show()
+  //classic javascript
+  document.getElementById().style.display = "block"
 }
 
 function hiddenwindow(){
-  console.log("ds")
+  //console.log("ds")
+  //реализации
+  //redu
   doc("window").s.hidden()
+  //classic javascript
+  document.getElementById().style.display = "none"
 }
